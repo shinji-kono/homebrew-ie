f2e429adcd8c659bdbbe3f8616162481e9c87752
Shinji KONO <kono@ie.u-ryukyu.ac.jp>
2020-08-11 13:31:11 +0900

    llvm build 5

--- cbc.rb
+++ cbc.rb
@@ -48,6 +48,16 @@ class Cbc < Formula
     sha256 "3839aab15f6d12495199fe380022b4374785a5d62262efbdeda3c4311164d696" => :mojave
     sha256 "3839aab15f6d12495199fe380022b4374785a5d62262efbdeda3c4311164d696" => :catalina
   end
+
+  bottle do
+    rebuild 5
+    root_url "http://www.cr.ie.u-ryukyu.ac.jp/brew" # Optional root to calculate bottle URLs
+    cellar :any
+    sha256 "237dd89a9f08bf85862c33f1fb685a25bb47f13a24e22a50fd1c5506781dc19b" => :mojave
+    sha256 "1ee4ef833dc2d123c7797210cd00566a5af5ae862dba603714cb2ebe57fefc15" => :catalina
+  end
+
+
   def pour_bottle?
     reason "The bottle needs the Xcode CLT to be installed."
     satisfy { MacOS::CLT.installed? }
